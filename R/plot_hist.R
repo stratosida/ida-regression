d3c60458dd1d924e8a44cb4d181bef55dc7fffee
bailliem <bailliem@gmail.com>
2020-06-01 09:53:00 +0200

    add reference lines with dodging

--- R/plot_hist.R
+++ R/plot_hist.R
@@ -1,14 +1,22 @@
+library(tidyverse)
 plot_hist <- function(data, var, bin_width = 0.1){
 
 min <- min(data %>% select({{var}}))  
 max <- max(data %>% select({{var}}))  
-mean <- mean(data %>% select({{var}})) 
+
+#mean <- data02 %>% 
+#  select({{var}}) %>% 
+#  summarise(mean({{var}})) 
+
+mean <- 30
+median <- 32
 
   data %>% 
     ggplot(aes({{var}})) +
     geom_histogram(binwidth = bin_width, alpha = 0.4) +
-    scale_x_continuous(limits = c(min,mean, max), 
-                       breaks= c(min, mean,max)) +
+    scale_x_continuous(limits = c(min,max), 
+                       breaks= c(min,median, mean, max),
+                       guide = guide_axis(n.dodge = 2)) +
     theme_minimal()
   
 }
