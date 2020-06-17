adaa0e0e6cc7aa3f554ed7529c5b4d245b3634b5
bailliem <bailliem@gmail.com>
2020-06-17 21:04:09 +0200

    WIP add to sc

--- R/plot_hist.R
+++ R/plot_hist.R
@@ -1,62 +1,80 @@
 #TODO: set up meta data
 
+#' Title
+#'
+#' @param data 
+#' @param var 
+#' @param bin_width 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 ida_plot_hist <- function(data, var, bin_width = 0.1){
   
-  breaks <- dplyr::summarise(data, 
-                             mean = mean({{var}}, na.rm = TRUE),
-                             median = median({{var}}, na.rm = TRUE),
-                             min = min({{var}}, na.rm = TRUE),
-                             max = max({{var}}, na.rm = TRUE),
-                             lower = quantile({{var}}, probs = 0.25, na.rm = TRUE),
-                             upper = quantile({{var}}, probs = 0.75, na.rm = TRUE)) 
-  
-  lim_labels <- breaks %>%
-    tidyr::pivot_longer(dplyr::everything(),
-                 names_to = "var",
-                 values_to = "val")
+
   
   p1 <- ggplot(data, aes(x = {{var}})) +
     geom_histogram(binwidth = bin_width, alpha = 0.7) +
     # geom_histogram(binwidth = function(x) 2 * IQR(x) / (length(x)^(1/3))) +
-    scale_x_continuous(limits = c(breaks$min, breaks$max), 
+#    scale_x_continuous(limits = c(breaks$min, breaks$max), 
                        #                       breaks = c(breaks$min, breaks$mean, breaks$median, breaks$max),
                        #                       guide = guide_axis(n.dodge = 2)
+    coord_cartesian(clip = "on") +
     ) +
     geom_rug() +
-    ggplot2::theme_minimal() +
-    theme(
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(), 
-      axis.title = element_blank(),
-      axis.text = element_blank()
-    )
+    ggplot2::theme_minimal() 
+#  +
+#    theme(
+#      panel.grid.major = element_blank(),
+#      panel.grid.minor = element_blank(), 
+#      axis.title = element_blank(),
+#      axis.text = element_blank()
+#   )
   
   
   p2 <- data %>% 
     ggplot(aes(x = {{var}}, y = factor(1))) + 
-    geom_boxplot() +
-    scale_x_continuous(
-      limits = c(breaks$min, breaks$max), 
-      breaks = c(breaks$min, breaks$mean, breaks$median, breaks$max),
-      guide = guide_axis(n.dodge = 2)
-    ) +
+    geom_jitter(alpha = 0.2, colour = "red") +
+  #  geom_boxplot() +
+#    scale_x_continuous(
+#      limits = c(breaks$min, breaks$max)
+#      , 
+#      breaks = c(breaks$min, breaks$mean, breaks$median, breaks$max),
+#      guide = guide_axis(n.dodge = 2)
+#    ) +
     ggplot2::theme_minimal()  
   
-  ## TODO: specify depenencies
+  p3 <- data %>% 
+    ggplot(aes(x = {{var}}, y = factor(1))) + 
+#    geom_jitter(alpha = 0.2, colour = "red") +
+    geom_boxplot() +
+    #    scale_x_continuous(
+    #      limits = c(breaks$min, breaks$max)
+    #      , 
+    #      breaks = c(breaks$min, breaks$mean, breaks$median, breaks$max),
+    #      guide = guide_axis(n.dodge = 2)
+    #    ) +
+    ggplot2::theme_minimal()  
+  
+  
+  
   layout <- c(
-    area(1, 1, 4, 3),
-    area(5, 1, 5, 3)
+    patchwork::area(1, 1, 4, 3),
+    patchwork::area(5, 1, 5, 3),
+    patchwork::area(6, 1, 6, 3)
   )
   
-  gg <- p1 / p2 + 
-    patchwork::plot_layout(design = layout) +
-    ggplot2::theme_minimal() +
-    theme(
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(), 
-      axis.title = element_blank(),
-      axis.text = element_blank()
-    )
+  gg <- p1 / p2 / p3 + 
+    patchwork::plot_layout(design = layout) 
+  #+
+  #  ggplot2::theme_minimal() +
+  #  theme(
+  #    panel.grid.major = element_blank(),
+  #    panel.grid.minor = element_blank(), 
+  #    axis.title = element_blank(),
+  #    axis.text = element_blank()
+  #  )
   
   
   
@@ -65,17 +83,71 @@ ida_plot_hist <- function(data, var, bin_width = 0.1){
 
 
 
-cacl_breaks <- function(data, var){
+breaks <- dplyr::summarise(data, 
+                           mean = mean({{var}}, na.rm = TRUE),
+                           median = median({{var}}, na.rm = TRUE),
+                           min = min({{var}}, na.rm = TRUE),
+                           max = max({{var}}, na.rm = TRUE),
+                           lower = quantile({{var}}, probs = 0.25, na.rm = TRUE),
+                           upper = quantile({{var}}, probs = 0.75, na.rm = TRUE)) 
+
+
+
+
+#' Title
+#'
+#' Calculate 5 number summary in wide format
+#'
+#' @param data 
+#' @param var 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_desc_summary <- function(data, var){
   breaks <- dplyr::summarise(data, 
+                             min = min({{var}}, na.rm = TRUE),
+                             Q1 = quantile({{var}}, 0.25, na.rm = TRUE),
                              mean = mean({{var}}, na.rm = TRUE),
                              median = median({{var}}, na.rm = TRUE),
-                             min = min({{var}}, na.rm = TRUE),
-                             max = max({{var}}, na.rm = TRUE),
-                             lower = quantile({{var}}, probs = 0.25, na.rm = TRUE),
-                             upper = quantile({{var}}, probs = 0.75, na.rm = TRUE)) 
+                             Q3 = quantile({{var}}, 0.75, na.rm = TRUE),
+                             max = max({{var}}, na.rm = TRUE)
+  ) 
   
-  return(breaks)
+  return(tibble::as_tibble(breaks))
 }
+
+
+
+#' Title
+#'
+#' Calculate 5 number summary in wide format
+#'
+#' @param data 
+#' @param var 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_summary_long <- function(data, var){
+  breaks <- dplyr::summarise(data, 
+                             min = min({{var}}, na.rm = TRUE),
+                             Q1 = quantile({{var}}, 0.25, na.rm = TRUE),
+                             mean = mean({{var}}, na.rm = TRUE),
+                             median = median({{var}}, na.rm = TRUE),
+                             Q3 = quantile({{var}}, 0.75, na.rm = TRUE),
+                             max = max({{var}}, na.rm = TRUE)
+  ) %>%
+    tidyr::pivot_longer(dplyr::everything(),
+                        names_to = "var",
+                        values_to = "value")
+  
+  
+  return(tibble::as_tibble(breaks))
+}
+
 
 
 
