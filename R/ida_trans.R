69db4c989f4e475fb659349e906442233129da75
Georg Heinze <georg.heinze@meduniwien.ac.at>
2021-03-05 17:13:25 +0100

    correct ida_trans.R

--- R/ida_trans.R
+++ R/ida_trans.R
@@ -24,7 +24,7 @@ ida_trans<-function(x, equ.marg=0.05){
   
   topt<-optimize(f=function(const) 1-cornorm(X=pseudo_log(x, sigma=const), qn=qn), interval=interval, tol=0.01)
   
-  if(topt$objective>(1-cn.orig+equ.marg)) {
+  if(topt$objective>(1-cn.orig-equ.marg)) {
     #cat("Original distribution better than any log.\n")
     res<-list(const=NA, fun=function(x) x)
   } else {
